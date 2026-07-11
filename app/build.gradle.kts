9b525844d67432b52d7a3ddd4d257c765cc8bfa1
redlotusgamesindia-star <redlotusgamesindia@gmail.com>
2026-07-11 18:45:34 +0530

    Bump versionCode to 2 in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.runtimelabs.clarity"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
